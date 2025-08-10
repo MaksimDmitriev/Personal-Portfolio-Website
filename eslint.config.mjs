359154e31f8b2f5104ee72b8d097ebd71e477960
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2025-08-10 16:07:21 +0800

    Add lint-like commands

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// eslint.config.mjs
+import js from "@eslint/js";
+import globals from "globals";
+import compat from "eslint-plugin-compat";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      compat, // declare once
+    },
+    rules: {
+      "compat/compat": "error",
+    },
+  },
+];
